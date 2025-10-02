74c82258e3162ff9331ec17806b224d1b683eb19
marcvoolpe <m.volpe.prignano.garcia@student.tue.nl>
2025-10-02 14:34:44 +0200

    refactor: change quality range to (1-100)

--- live_bargaining/constants.py
+++ live_bargaining/constants.py
@@ -43,7 +43,7 @@ class C(RoleConstants):
     PRICE_MIN = 1.0
     PRICE_MAX = 14.0
     PRICE_RANGE = [x * 0.01 for x in range(100, 1401)]  # Generates 1.00 to 14.00 with a step of 0.01
-    QUALITY_RANGE = range(0, 100)
+    QUALITY_RANGE = range(1, 101)
     DEMAND_MIN = min(QUALITY_RANGE)
     DEMAND_MAX = max(QUALITY_RANGE)
 
